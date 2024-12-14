903392ba98e2c83a820bc45f58023e9597a1f516
maundu55 <marymaundu.mm@gmail.com>
2024-12-14 06:51:32 +0300

    add assests

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,10 +1,15 @@
-import { View, Text, StyleSheet} from 'react-native'
+import { View, Text, StyleSheet, ImageBackground} from 'react-native'
 import React from 'react'
+import icedCoffeeImg  from '@/assets/images/iced-coffee.png'
 
 const app = () => {
   return (
     <View style={styles.container}>
+      <ImageBackground
+      source={icedCoffeeImg}
+      >
       <Text style={styles.text}>Coffee shop</Text>
+      </ImageBackground>
     </View>
   )
 }
